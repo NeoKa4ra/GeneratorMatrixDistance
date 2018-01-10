fd2bf34d26293f219ca8415ba2abaddc8ed2a3d3
Rom <navarro.romain.j@gmail.com>
2018-01-10 20:58:35 +0100

    Commenting the Generator

--- src/Distance/Generator.java
+++ src/Distance/Generator.java
@@ -4,7 +4,7 @@ package Distance;
 public class Generator {
 
 	public static void main(String[] args) {
-		
+
 		// Number of destinations in a square matrix
 		int nbDestSym = 10;
 		// Number of participants of the carpooling
@@ -16,12 +16,26 @@ public class Generator {
 		// range of the last DEPARTURE in the dates
 		int rangeDates = 1000;
 
+		// Usage :
+		// Time() : Generate a random symmetric square matrix by default
+		// Time(n) : Generate a random symmetric square matrix[n][n]
+		// Time(n, r) : Generate a random symmetric matrix[n][n] and the range r
+		// of the random numbers
+		// Time(n, r, d) : Generate a random symmetric matrix[n][n], the range r
+		// of the random numbers, and a given diagonal
 		Time t = new Time(nbDestSym, distanceMax);
 		t.printMatrix();
 
+		// Usage :
+		// Route(n, p) : Create a route among n destinations for p participants
 		Route r = new Route(nbDestSym, nbPart);
 		r.printRoute();
 
+		// Usage :
+		// Date(p,d) : Create a date for p participants having at least the time
+		// to do d distance
+		// Date(p,d,r) : Create a date for p participants having at least the
+		// time to do d distance, and the DEPARTURE range is between 1 and r
 		Date d = new Date(nbPart, distanceMax, rangeDates);
 		d.printDate();
 
